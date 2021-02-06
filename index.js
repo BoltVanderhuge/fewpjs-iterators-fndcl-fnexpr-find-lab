3c1201feb3f9bc1c422f3279df9f97e7993545c5
john_wisneski <john.j.wisneski@gmail.com>
2021-02-05 22:45:56 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+// function superbowlWin(record) {
+//   result = record.find(reco => reco.result === "W")
+//     if (result){
+//       return result.year
+    
+//     } else {
+//       return undefined
+//     }
+// }
+superbowlWin = (record) => {
+  let result = record.find( record => record.result === "W" )
+  return !!result ? result.year : undefined
 }
